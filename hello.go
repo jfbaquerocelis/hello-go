c0755643e3ae08a7cc57a56d24b762faef206fce
John Baquero Celis <jfbaquerocelis@gmail.com>
2019-06-05 07:59:29 -0500

    [add] my first library

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jfbaquerocelis/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello World!!")
+	fmt.Println(stringutil.Reverse("YVAN EHT NIOJ"))
 }
